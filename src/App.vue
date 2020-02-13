75e25b0bfe1b0aaa5f3b80c987f9556199d96526
Anais <anais.schlienger@mines-paristech.fr>
2020-02-13 12:33:25 +0100

    Ajout src

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <BattleBoard></BattleBoard>
+  </div>
+</template>
+
+<script>
+import BattleBoard from "./components/BattleBoard.vue";
+export default {
+  name: "App",
+  components: { BattleBoard }
+};
+</script>
+
+<style lang="scss" scoped></style>
